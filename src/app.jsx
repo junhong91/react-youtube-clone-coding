7e3f1242b0d062c5954c70fdbc00bda0f0865c0f
junhong91 <junhong199@gmail.com>
2022-08-10 17:36:28 +0900

    Impl onSearch Handler

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,32 @@
+// import "./app.css";
+// import Navbar from "./components/navbar";
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar onSearch={handleSearch} />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from "react";
 import "./app.css";
 import Navbar from "./components/navbar";
 
-function App() {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
+class App extends Component {
+  handleSearch(keyword) {
+    console.log(keyword);
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar onSearch={this.handleSearch} />
+      </>
+    );
+  }
 }
 
 export default App;
